4ae9820ceb03dc681de232e63254596fabb63273
Naoya Yamashita <conao3@gmail.com>
2021-10-31 20:35:12 +0900

    formatter

--- src/ergen/main.py
+++ src/ergen/main.py
@@ -7,6 +7,7 @@ current_dir = pathlib.Path(__file__).resolve().parent
 fileSystemLoader = jinja2.FileSystemLoader(searchpath=current_dir / "template")
 env = jinja2.Environment(loader=fileSystemLoader)
 
+
 def main(data: types.ErdType):
     template = env.get_template('erd.dot.jinja')
     print(template.render(**data.dict()))
